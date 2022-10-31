009de62049f8178f4185dacab3027a14d8700eb2
Kim Byung Jun <celbever@naver.com>
2022-10-31 19:49:13 +0900

    [2022_10_31] 백준 n 퀸 개선

--- src/baekjoon/_9663/Main.java
+++ src/baekjoon/_9663/Main.java
@@ -7,7 +7,6 @@ import java.io.InputStreamReader;
 public class Main {
     //9663 N퀸
     private static int n;
-    private static boolean[] rowCheck;
     private static boolean[] columnCheck;
     private static boolean[] lToRUpDiagonal;
     private static boolean[] lToRDownDiagonal;
@@ -36,45 +35,37 @@ public class Main {
 
 
     private static void setPosition(int x, int y) {
-        rowCheck[y] = true;
         columnCheck[x] = true;
         lToRUpDiagonal[x + y] = true;
         lToRDownDiagonal[n - 1 - y + x] = true;
-
     }
 
 
     private static boolean canSetPosition(int x, int y) {
-        return !(isRowCheck(y) || isColumnCheck(x) || isLToRUpDiagonalCheck(x, y) || isLToRDownDiagonalCheck(x, y));
+        return canSetColumn(x) && canSetLToRUpDiagonal(x,y) && canSetLToRDownDiagonal(x, y);
     }
 
     private static void resetPosition(int x, int y) {
-        rowCheck[y] = false;
         columnCheck[x] = false;
         lToRUpDiagonal[x + y] = false;
         lToRDownDiagonal[n - 1 - y + x] = false;
     }
 
-    private static boolean isRowCheck(int y) {
-        return rowCheck[y];
+    private static boolean canSetColumn(int x) {
+        return !columnCheck[x];
     }
 
-    private static boolean isColumnCheck(int x) {
-        return columnCheck[x];
+    private static boolean canSetLToRUpDiagonal(int x, int y) {
+        return !lToRUpDiagonal[x + y];
     }
 
-    private static boolean isLToRUpDiagonalCheck(int x, int y) {
-        return lToRUpDiagonal[x + y];
-    }
-
-    private static boolean isLToRDownDiagonalCheck(int x, int y) {
-        return lToRDownDiagonal[n - 1 - y + x];
+    private static boolean canSetLToRDownDiagonal(int x, int y) {
+        return !lToRDownDiagonal[n - 1 - y + x];
     }
 
     private static void read() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
-        rowCheck = new boolean[n];
         columnCheck = new boolean[n];
         lToRUpDiagonal = new boolean[n * 2 - 1];
         lToRDownDiagonal = new boolean[n * 2 - 1];
